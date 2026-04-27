389a3c1987f255b756ed9ff65e15ebb0fc934b06
Your Name <you@example.com>
2026-04-27 15:07:39 +0300

    Заполнил логгерами 2 модуля 12_2 от 27.04.2026

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,22 @@
+import os
+import logging
+from idlelib.iomenu import encoding
+
+# Поднимаемся на уровень выше из src в корень проекта
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+logs_path = os.path.join(project_root, 'logs', 'logger_masks.log')
+
+logger = logging.getLogger('logger_masks')
+file_handler = logging.FileHandler(logs_path, mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s, %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
+    logger.info('Проверяем условия соответствия номера карты.')
     if not card_number:
         return "Номер карты не введен."
     if len(card_number) > 16:
@@ -11,17 +28,20 @@ def get_mask_card_number(card_number: str) -> str:
     else:
         mask_number = card_number[:6] + "XXXXXX" + card_number[12:]
         groups = []
+        logger.info('Приводим номер карты к соответствующему образцу вывода.')
         for i in range(0, len(mask_number), 4):
             group = mask_number[i: i + 4]
             groups.append(group)
-
+        logger.info('Выводим результат номера карты, приведенного в соответствие.')
         result = " ".join(groups)
         return result
+
+get_mask_card_number('2345347865435679')
 
 
 def get_mask_account(mask_account: str) -> str:
     """ "Функция, которая укорачивает и маскирует номер счета"""
-
+    logger.info('Проверяем соответствие номера счета критериям.')
     if not mask_account:
         return "Вы не ввели номер счета."
     if len(mask_account) > 20:
@@ -31,6 +51,11 @@ def get_mask_account(mask_account: str) -> str:
     if not mask_account.isdigit():
         return "Номер счета должен состоять только из цифр."
     else:
+        logger.info('Проверка соответствия номера счета пройдена, проводится редактирования номера карты'
+                    'для вывода.')
         new_mask_account = mask_account[-6:]
         result = new_mask_account.replace(new_mask_account[0:2], "**")
+        logger.info('Выполнение формирования номера счета завершена.')
         return result
+
+get_mask_account('34543678756894567467')
--- src/utils/external_api.py
+++ src/utils/external_api.py
@@ -2,6 +2,20 @@ import json
 import os
 import requests
 from dotenv import load_dotenv
+import logging
+
+# Поднимаемся на уровень выше из src в корень проекта
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+logs_path = os.path.join(project_root, 'logs', 'external_log.log')
+
+
+logger = logging.getLogger('external_log')
+file_handler = logging.FileHandler(logs_path, mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s, %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
 
 file_path = os.path.join('..', '..', 'data', 'operations.json')
 
@@ -13,11 +27,17 @@ def operations_json(json_file: str) -> list:
     if not os.path.exists(json_file):
         return []
     try:
+        logger.info('Осуществляется прием json файла со списком транзакций, '
+                    'обрабатывается и выводит список транзакций в виде списка словарей.')
         with open(json_file, 'r', encoding='utf-8') as f:
             data = json.load(f)
             if len(data) == 0:
                 return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        logger.error(f'Ошибка парсинга JSON: {e}')
+        return []
+    except IOError as e:
+        logger.error(f'Ошибка чтения файла: {e}')
         return []
     return data
 
@@ -32,6 +52,7 @@ def covert_to_rub(list_transactions: dict) -> float:
     currency_code = list_transactions["operationAmount"]["currency"]["code"]
     amount = float(list_transactions["operationAmount"]["amount"])
     try:
+        logger.info('Идет процесс получения сведений по курсам валют.')
         if currency_code != "RUB":
             url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={amount}"
             headers = {
@@ -40,10 +61,13 @@ def covert_to_rub(list_transactions: dict) -> float:
             response = requests.get(url, headers=headers)
             response_json = response.json()
             converted_amount = response_json["result"]
+            logger.info('Получены сведения по курсам валют.')
             return float(converted_amount)
         else:
+            logger.info('Выведена сумма в рублях без перевода курса валют.')
             return amount
-    except requests.exceptions.RequestException:
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Возникла ошибка при выведении суммы. Ошибка: {e}')
         print("Возникла ошибка!")
         return 0.0
 
